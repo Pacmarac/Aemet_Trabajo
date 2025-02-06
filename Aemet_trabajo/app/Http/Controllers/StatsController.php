d0c3ac7fa645d5fbcbf49fc3442328a5d9a54ede
Pacmarac <siporquecopon@gmail.com>
2025-02-06 17:36:32 +0100

    RecolectaInv
    me falta subir a la base de datos, pero ya pilla las estaciones sin problemas

--- Aemet_trabajo/app/Http/Controllers/StatsController.php
+++ Aemet_trabajo/app/Http/Controllers/StatsController.php
@@ -3,15 +3,35 @@
 namespace App\Http\Controllers;
 use App\Models\Dato;
 use Illuminate\Http\Request;
+use Http;
+
 
 class StatsController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function recolectaStat()
+    public function recolectaInv()
     {
-    
+        $url = "https://opendata.aemet.es/opendata/api/valores/climatologicos/inventarioestaciones/todasestaciones";
+        $response = Http::withHeaders([
+            "API_KEY" => env("API_KEY")
+        ])->get($url)["datos"];
+        $datas = Http::get($response);
+        $datas = mb_convert_encoding($datas,"UTF-8", "auto");
+        $datajson = json_decode($datas, true);
+         foreach( $datajson as $valor){
+            $info[] = [
+                'nombre' => $valor['nombre'] ?? null,
+                'idema'    => $valor['indicativo'] ?? null,
+                'provincia'    => $valor['provincia'] ?? null,  // Asigna null si no existe 'ta'
+                'latitud'    => $valor['latitud'] ?? null,
+                'longitud'  => $valor['longitud'] ?? null,
+                'altitud'  => $valor['altitud'] ?? null
+            ];
+        }; 
+        return $info;
+       
     }
 
     /**
--- Aemet_trabajo/routes/web.php
+++ Aemet_trabajo/routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Http;
 
 Route::get('/', function () {
-    echo 'Que haces aqui esto no es donde deberias estar no vayas de chuklito y vuelve a tu pagina.';
+    return view('welcome');
 });
